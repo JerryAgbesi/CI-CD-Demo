48f249c226b6921d054cbbaf77a35dc78e7993b9
Jerry Agbesi <jerryeagbesi@gmail.com>
2024-08-16 18:32:03 +0000

    feat: add image build and push stage

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,5 +1,9 @@
 pipeline {
-  agent any
+    agent any
+
+    environment{
+        imagename = "jerryelikem/ci-cd-demo:latest"
+    }
 
     stages{
 
@@ -9,11 +13,50 @@ pipeline {
             }
         }
 
-        stage("Build"){
+        stage("Static code analysis"){
             steps{
-                sh "docker build . -t ci-cd-demo:v1" 
+                def scannerHome = tool 'SonarScanner';
+                withSonarQubeEnv('SonarQube') {
+                sh "${scannerHome}/bin/sonar-scanner"
+                }
+            }
+        }
+
+        stage("Build image"){
+            steps{
+                sh "docker build . -t ${imagename}" 
+            }
+        }
+
+        stage("Push image to registry"){
+            steps{
+                 script {
+                    // Use Jenkins credentials for Docker Hub login
+                    withCredentials([usernamePassword(credentialsId: demoDockerHubCredentials, usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
+                        sh "docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD"
+ 
+                        // Push the image
+                        sh "docker push ${imagename}"
+                    }
+            }
+
             }
         }
     }
+
+    post{
+        always{
+            script {
+                try {
+                    sh "docker rmi ${imagename}"
+                    sh 'docker system prune -f'
+                    cleanWs()
+                } catch (Exception e) {
+                    echo "Cleanup failed: ${e.getMessage()}"
+                }
+            }
+        }
+    }
+
 
 }
--- src/routes/books.py
+++ src/routes/books.py
@@ -12,17 +12,57 @@ appRouter = APIRouter(prefix="/books", tags=["books"])
 
 
 @appRouter.post("/",status_code=status.HTTP_201_CREATED,response_model=BookModel)
-def create_book(student:BookCreate ,db: Session = Depends(get_db)):
-    new_book = Book(**student.model_dump())
+def create_book(book:BookCreate ,db: Session = Depends(get_db)):
+    new_book = Book(**book.model_dump())
     try:
         db.add(new_book)
         db.commit()
         db.refresh(new_book)
 
         logging.info("new book created")
-        
+
         return new_book
     except Exception as e:
         db.rollback()
         logging.debug(e)
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="Unable to create book")
+
+@appRouter.get("/{book_id}",status_code=status.HTTP_200_OK,response_model=BookModel)
+def get_book(book_id:int,db: Session = Depends(get_db)):
+    book = db.query(Book).filter(Book.id == book_id).first()
+    if not book:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Book not found")
+    
+    return book
+
+@appRouter.patch("/{book_id}",status_code=status.HTTP_200_OK,response_model=BookModel)
+def update_book(book_id:int,book:BookUpdate,db: Session = Depends(get_db)):
+    get_book =  db.query(Book).filter(Book.id == book_id)
+    get_book_object = get_book.first()
+    if not get_book_object:
+        raise HTTPException(status_code=404,detail="book not found")
+    
+
+    
+    update_data = book.model_dump(exclude_unset=True)
+    # new_update = {k: v for k, v in update_data.items() if v is not None}
+
+    for key, value in update_data.items():
+        setattr(get_book_object,key,value)  
+    
+    # get_book.update(new_update)
+    
+    
+    db.commit()
+
+    return get_book.first()
+
+@appRouter.delete("/{book_id}",status_code=status.HTTP_204_NO_CONTENT)
+def delete_book(book_id:int,db: Session = Depends(get_db)):
+    get_book =  db.query(Book).filter(Book.id == book_id)
+    if not get_book.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Book not found")
+    
+    get_book.delete(synchronize_session=False)
+    db.commit()
+    return None
